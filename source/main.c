8d3ed6ac4a00639f4268867a94365105ec9c9016
Demarcus Sales <dthecsguy@gmail.com>
2020-08-20 21:04:10 -0700

    Part 1

--- source/main.c
+++ source/main.c
@@ -117,16 +117,18 @@ int main(void) {
     /* Insert your solution below */
     while (1) {
 		for(unsigned char i = 0; i < tasksNum; ++i){
-		if (tasks[i].elapsedTime >= tasks[i].period){
-			tasks[i].state = tasks[i].TickFct(tasks[i].state);
-			tasks[i].elapsedTime = 0;
+			if (tasks[i].elapsedTime >= tasks[i].period){
+				tasks[i].state = tasks[i].TickFct(tasks[i].state);
+				tasks[i].elapsedTime = 0;
+			}
+			
+			tasks[i].elapsedTime += tasksPeriodGCD;
 		}
-		tasks[i].elapsedTime += tasksPeriodGCD;
-	
+		
 		set_out();
 		
 		while(!TimerFlag){}
 		TimerFlag = 0;
-    }
+	}
     return 1;
 }
